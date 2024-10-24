865cef801647bf73b688c426d5a5acb3d790d6a3
Juslin Jukka <jusju@haaga-helia.fi>
2024-10-24 13:11:35 +0300

    libraries fixed

--- src/main/java/fi/haagahelia/course/StudentListSignUpApplication.java
+++ src/main/java/fi/haagahelia/course/StudentListSignUpApplication.java
@@ -27,9 +27,9 @@ public class StudentListSignUpApplication {
 	public CommandLineRunner studentDemo(StudentRepository srepository, DepartmentRepository drepository, AppUserRepository urepository) {
 		return (args) -> {
 			log.info("save a couple of students");
-			//drepository.save(new Department("IT"));
-			//drepository.save(new Department("Business"));
-			//drepository.save(new Department("Law"));
+			drepository.save(new Department("IT"));
+			drepository.save(new Department("Business"));
+			drepository.save(new Department("Law"));
 			
 			//srepository.save(new Student("John", "Johnson", "john@john.com", drepository.findByName("IT").get(0)));
 			//srepository.save(new Student("Katy", "Kateson", "kate@kate.com", drepository.findByName("Business").get(0)));	
@@ -37,8 +37,8 @@ public class StudentListSignUpApplication {
 			// Create users: admin/admin user/user
 			AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
 			AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
-			//urepository.save(user1);
-			//urepository.save(user2);
+			urepository.save(user1);
+			urepository.save(user2);
 			
 			log.info("fetch all students");
 			for (Student student : srepository.findAll()) {
